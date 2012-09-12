2613e26777c970183abce78df9219795f32b5962
wwq0327 <wwq0327@gmail.com>
2012-09-12 21:09:00 +0800

    f

---
+++ lrthw/ex5.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+my_name = 'Wyatt, wang'
+my_age = 33
+my_height = 74
+my_weight = 180
+my_eyes = "black"
+my_teeth= "yellow"
+my_hair = "black"
+
+puts "Let's talk about %s." % my_name
+puts "He's %d inches tall." % my_height
+puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
