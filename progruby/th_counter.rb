9f733ac33d8cdddcfc5efa9890e3225c4ee29f94
wwq0327 <wwq0327@gmail.com>
2014-06-02 09:03:19 +0800

    threads

---
+++ progruby/th_counter.rb
@@ -0,0 +1,16 @@
+class Counter
+  attr_reader :count
+  def initialize
+    @count = 0
+  end
+  def tick
+    @count += 1
+  end
+end
+
+c = Counter.new
+t1 = Thread.new { 100_000.times { c.tick } }
+t2 = Thread.new { 100_000.times { c.tick } }
+t1.join
+t2.join
+puts c.count
